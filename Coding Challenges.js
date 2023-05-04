7ede718c4f938613f654a3b82010b2e5824d4382
jldees2s <117608836+jldees2s@users.noreply.github.com>
2023-05-04 17:43:08 -0500

    Add files via upload

---
+++ Coding Challenges.js
@@ -0,0 +1,11 @@
+//Coding Challenge 1
+
+function triArea(base, height) {  return base * height/2;}
+
+//Coding Challenge 2
+function hello() { return "hello edabit.com"}
+
+//Coding Challenge 3
+function FirstReverse(str) {  return str.split('').reverse().join('');}
+
+//Coding Challenge 4
